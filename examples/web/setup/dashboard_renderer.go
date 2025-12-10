557419e6e865e030c0021da2c1690af1baa45810
goliatone <140582+goliatone@users.noreply.github.com>
2025-12-10 11:10:25 -0800

    chore: update example

---
+++ examples/web/setup/dashboard_renderer.go
@@ -0,0 +1,13 @@
+package setup
+
+import (
+	"github.com/goliatone/go-admin/admin"
+	"github.com/goliatone/go-admin/examples/web/renderers"
+)
+
+// NewDashboardRenderer creates a template-based dashboard renderer
+func NewDashboardRenderer() (admin.DashboardRenderer, error) {
+	// Pass the templates directory (go-template expects a directory, not a file)
+	tmplDir := "templates"
+	return renderers.NewTemplateRenderer(tmplDir)
+}
