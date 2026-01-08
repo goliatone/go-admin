f8731e3e91dc1fb1ff02d43409d923ebc80b4d01
goliatone <140582+goliatone@users.noreply.github.com>
2026-01-07 18:51:14 -0800

    refactor: move templates to client

--- examples/web/setup/dashboard_renderer.go
+++ examples/web/setup/dashboard_renderer.go
@@ -3,11 +3,10 @@ package setup
 import (
 	"github.com/goliatone/go-admin/examples/web/renderers"
 	"github.com/goliatone/go-admin/pkg/admin"
+	"github.com/goliatone/go-admin/pkg/client"
 )
 
 // NewDashboardRenderer creates a template-based dashboard renderer
 func NewDashboardRenderer() (admin.DashboardRenderer, error) {
-	// Pass the templates directory (go-template expects a directory, not a file)
-	tmplDir := "templates"
-	return renderers.NewTemplateRenderer(tmplDir)
+	return renderers.NewTemplateRenderer(client.Templates())
 }
