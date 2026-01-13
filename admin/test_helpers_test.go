5acb046a52e23f2a0af10033d59030e54d4808d5
goliatone <140582+goliatone@users.noreply.github.com>
2026-01-13 14:27:37 -0800

    chore: update tests

--- admin/test_helpers_test.go
+++ admin/test_helpers_test.go
@@ -1,12 +1,23 @@
 package admin
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/goliatone/go-command/registry"
+)
 
 func mustNewAdmin(t *testing.T, cfg Config, deps Dependencies) *Admin {
 	t.Helper()
+	_ = registry.Stop(context.Background())
 	adm, err := New(cfg, deps)
 	if err != nil {
 		t.Fatalf("admin.New: %v", err)
+	}
+	if adm != nil && adm.Commands() != nil {
+		t.Cleanup(func() {
+			adm.Commands().Reset()
+		})
 	}
 	return adm
 }
