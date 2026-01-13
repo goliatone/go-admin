1dee31a14c19443c2defde0ef5fdcf89f6075ab3
goliatone <140582+goliatone@users.noreply.github.com>
2026-01-13 14:00:59 -0800

    refactor: command implementation

--- examples/web/jobs/jobs.go
+++ examples/web/jobs/jobs.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	"github.com/goliatone/go-admin/examples/web/stores"
+	"github.com/goliatone/go-command"
+	"github.com/goliatone/go-command/dispatcher"
 )
 
 // databaseBackupJob performs database backups
@@ -21,20 +23,20 @@ func (j *databaseBackupJob) Name() string {
 	return "jobs.database_backup"
 }
 
-func (j *databaseBackupJob) Execute(ctx context.Context) error {
+func (j *databaseBackupJob) Execute(ctx context.Context, _ DatabaseBackupMsg) error {
 	j.runCount++
 	log.Printf("Running database backup (run #%d)...", j.runCount)
 	return nil
 }
 
-func (j *databaseBackupJob) CronSpec() string {
-	return "@daily"
-}
-
 func (j *databaseBackupJob) CronHandler() func() error {
 	return func() error {
-		return j.Execute(context.Background())
+		return dispatcher.Dispatch(context.Background(), DatabaseBackupMsg{})
 	}
+}
+
+func (j *databaseBackupJob) CronOptions() command.HandlerConfig {
+	return command.HandlerConfig{Expression: "@daily"}
 }
 
 // cacheCleanupJob performs cache cleanup
@@ -49,19 +51,19 @@ func (j *cacheCleanupJob) Name() string {
 	return "jobs.cache_cleanup"
 }
 
-func (j *cacheCleanupJob) Execute(ctx context.Context) error {
+func (j *cacheCleanupJob) Execute(ctx context.Context, _ CacheCleanupMsg) error {
 	log.Println("Cleaning up cache...")
 	return nil
 }
 
-func (j *cacheCleanupJob) CronSpec() string {
-	return "@hourly"
-}
-
 func (j *cacheCleanupJob) CronHandler() func() error {
 	return func() error {
-		return j.Execute(context.Background())
+		return dispatcher.Dispatch(context.Background(), CacheCleanupMsg{})
 	}
+}
+
+func (j *cacheCleanupJob) CronOptions() command.HandlerConfig {
+	return command.HandlerConfig{Expression: "@hourly"}
 }
 
 // contentExportJob exports content
@@ -78,19 +80,19 @@ func (j *contentExportJob) Name() string {
 	return "jobs.content_export"
 }
 
-func (j *contentExportJob) Execute(ctx context.Context) error {
+func (j *contentExportJob) Execute(ctx context.Context, _ ContentExportMsg) error {
 	log.Println("Exporting content...")
 	return nil
 }
 
-func (j *contentExportJob) CronSpec() string {
-	return "@weekly"
-}
-
 func (j *contentExportJob) CronHandler() func() error {
 	return func() error {
-		return j.Execute(context.Background())
+		return dispatcher.Dispatch(context.Background(), ContentExportMsg{})
 	}
+}
+
+func (j *contentExportJob) CronOptions() command.HandlerConfig {
+	return command.HandlerConfig{Expression: "@weekly"}
 }
 
 // inactiveUsersCleanupJob cleans up inactive users
@@ -107,17 +109,17 @@ func (j *inactiveUsersCleanupJob) Name() string {
 	return "jobs.inactive_users_cleanup"
 }
 
-func (j *inactiveUsersCleanupJob) Execute(ctx context.Context) error {
+func (j *inactiveUsersCleanupJob) Execute(ctx context.Context, _ InactiveUsersCleanupMsg) error {
 	log.Println("Cleaning up inactive users...")
 	return nil
 }
 
-func (j *inactiveUsersCleanupJob) CronSpec() string {
-	return "@monthly"
-}
-
 func (j *inactiveUsersCleanupJob) CronHandler() func() error {
 	return func() error {
-		return j.Execute(context.Background())
+		return dispatcher.Dispatch(context.Background(), InactiveUsersCleanupMsg{})
 	}
+}
+
+func (j *inactiveUsersCleanupJob) CronOptions() command.HandlerConfig {
+	return command.HandlerConfig{Expression: "@monthly"}
 }
