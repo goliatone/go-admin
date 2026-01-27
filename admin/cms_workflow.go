be21332271afa38416dd0daa4d074933d80f3c49
goliatone <140582+goliatone@users.noreply.github.com>
2026-01-27 08:43:37 -0800

    add: admin cms workflow

---
+++ admin/cms_workflow.go
@@ -0,0 +1,47 @@
+package admin
+
+func resolveCMSWorkflowEngine(a *Admin) WorkflowEngine {
+	if a == nil {
+		return nil
+	}
+	if a.workflow != nil {
+		return a.workflow
+	}
+	engine := NewSimpleWorkflowEngine()
+	registerCMSWorkflow(engine)
+	return engine
+}
+
+func registerCMSWorkflow(engine *SimpleWorkflowEngine) {
+	if engine == nil {
+		return
+	}
+	definition := WorkflowDefinition{
+		EntityType:   "content",
+		InitialState: "draft",
+		Transitions: []WorkflowTransition{
+			{
+				Name:        "submit_for_approval",
+				Description: "Submit for approval",
+				From:        "draft",
+				To:          "approval",
+			},
+			{
+				Name:        "publish",
+				Description: "Publish",
+				From:        "approval",
+				To:          "published",
+			},
+		},
+	}
+	engine.RegisterWorkflow("content", definition)
+	definition.EntityType = "pages"
+	engine.RegisterWorkflow("pages", definition)
+}
+
+func cmsWorkflowActions() []Action {
+	return []Action{
+		{Name: "submit_for_approval", Label: "Submit for approval"},
+		{Name: "publish", Label: "Publish"},
+	}
+}
