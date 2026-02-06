f0b73f8f4be672d3c738f6eb39dc66b5c232d710
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-05 21:23:44 -0800

    chore: update examples

--- examples/web/handlers/password_hash.go
+++ examples/web/handlers/password_hash.go
@@ -1,5 +1,38 @@
 package handlers
 
-import authlib "github.com/goliatone/go-auth"
+import (
+	"flag"
+	"os"
+	"strings"
+
+	authlib "github.com/goliatone/go-auth"
+	"golang.org/x/crypto/bcrypt"
+)
 
 var hashPassword = authlib.HashPassword
+
+func init() {
+	if isTestRun() {
+		hashPassword = fastHashPassword
+	}
+}
+
+func fastHashPassword(password string) (string, error) {
+	if password == "" {
+		return "", authlib.ErrNoEmptyString
+	}
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	return string(hashed), err
+}
+
+func isTestRun() bool {
+	if flag.Lookup("test.v") != nil {
+		return true
+	}
+	for _, arg := range os.Args {
+		if strings.HasPrefix(arg, "-test.") {
+			return true
+		}
+	}
+	return strings.HasSuffix(os.Args[0], ".test")
+}
