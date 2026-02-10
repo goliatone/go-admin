b37be31d5b220b09aa8a1f01b5403839a815e53a
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-09 22:49:41 -0800

    add: data migration

--- data/data.go
+++ data/data.go
@@ -5,12 +5,21 @@ import (
 	"io/fs"
 )
 
-//go:embed uischemas
+//go:embed uischemas sql/migrations/*.sql sql/migrations/sqlite/*.sql sql/migrations/postgres/*.sql
 var embeddedFS embed.FS
 
 // UISchemas returns the embedded UI schema filesystem.
 func UISchemas() fs.FS {
 	sub, err := fs.Sub(embeddedFS, "uischemas")
+	if err != nil {
+		return embeddedFS
+	}
+	return sub
+}
+
+// TranslationAssignmentMigrations returns queue assignment migrations rooted at sql/migrations.
+func TranslationAssignmentMigrations() fs.FS {
+	sub, err := fs.Sub(embeddedFS, "sql/migrations")
 	if err != nil {
 		return embeddedFS
 	}
