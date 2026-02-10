c534a03d2e9e21574f6584bbde02e89127d2ffe3
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-09 22:21:12 -0800

    add: translation workflow

--- admin/feature_flag_keys.go
+++ admin/feature_flag_keys.go
@@ -24,6 +24,7 @@ func defaultFeatureFlagKeys() []string {
 		string(FeatureUsers),
 		string(FeatureTenants),
 		string(FeatureOrganizations),
+		string(FeatureTranslationExchange),
 		"debug",
 		"users.invite",
 		fggate.FeatureUsersSignup,
