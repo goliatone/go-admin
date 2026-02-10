c545cfb25598ee5b6d149fd02867904bf494c6f2
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-09 22:18:26 -0800

    add: feature check for translation exchagne

--- admin/bootstrap.go
+++ admin/bootstrap.go
@@ -158,6 +158,7 @@ func (a *Admin) validateConfig() error {
 
 	require(FeatureJobs, FeatureCommands)
 	require(FeatureBulk, FeatureCommands, FeatureJobs)
+	require(FeatureTranslationExchange, FeatureCommands, FeatureCMS)
 
 	for _, feature := range []FeatureKey{FeatureMedia, FeatureExport, FeatureBulk} {
 		if featureEnabled(a.featureGate, feature) && !featureEnabled(a.featureGate, FeatureCMS) {
