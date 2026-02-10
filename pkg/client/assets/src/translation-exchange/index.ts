8a85d692f34af1f3b0b7ed671fd3cd8583b0ca52
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-09 22:43:07 -0800

    add: translation exchange ui

---
+++ pkg/client/assets/src/translation-exchange/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Translation Exchange Module
+ * UI for managing translation export/import workflows
+ */
+
+export { TranslationExchangeManager } from './translation-exchange-manager.js';
+
+export type {
+  TranslationExchangeConfig,
+  TranslationExchangeSelectors,
+  LocaleOption,
+  ResourceOption,
+  ExportRequest,
+  ExportResponse,
+  ExportRow,
+  ImportOptions,
+  ImportResult,
+  ImportState,
+  RowResult,
+  RowStatus,
+  ResultSummary,
+  ToastNotifier,
+} from './types.js';
