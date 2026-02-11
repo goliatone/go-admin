fca9ae57509a3fe21a5677f62794baa71c72bb42
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-11 14:44:02 -0800

    add: cms content list ootpions using projected fields

--- admin/cms_content_list_options.go
+++ admin/cms_content_list_options.go
@@ -5,9 +5,15 @@ package admin
 type CMSContentListOption = string
 
 const cmsContentListWithTranslations CMSContentListOption = "content:list:with_translations"
+const cmsContentListWithDerivedFields CMSContentListOption = "content:list:projection:derived_fields"
 
 // WithTranslations requests that list operations preload translations for CMS content.
 // This opt-in token mirrors the go-cms list option of the same name.
 func WithTranslations() CMSContentListOption {
 	return cmsContentListWithTranslations
+}
+
+// WithDerivedFields requests canonical top-level derived content fields from go-cms.
+func WithDerivedFields() CMSContentListOption {
+	return cmsContentListWithDerivedFields
 }
