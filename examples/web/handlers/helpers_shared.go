09f3f6d482a59700f4350f67ad16cfab964ed258
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-18 11:16:16 -0800

    refactor: clean up code

--- examples/web/handlers/helpers_shared.go
+++
@@ -1,5 +0,0 @@
-package handlers
-
-import "github.com/goliatone/go-admin/internal/primitives"
-
-var cloneAnyMap = primitives.CloneAnyMapEmptyOnEmpty
--- examples/web/stores/helpers_shared.go
+++
@@ -1,5 +0,0 @@
-package stores
-
-import "github.com/goliatone/go-admin/internal/primitives"
-
-var cloneAnyMap = primitives.CloneAnyMapNilOnEmpty
