3e32d70eb8883900df4cde54ce15728e70c6c139
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-18 10:00:59 -0800

    refactor: make code more compact

--- quickstart/util.go
+++ quickstart/util.go
@@ -1,38 +1,10 @@
 package quickstart
 
-import "strings"
+import "github.com/goliatone/go-admin/internal/primitives"
 
-func firstNonEmpty(values ...string) string {
-	for _, v := range values {
-		if strings.TrimSpace(v) != "" {
-			return strings.TrimSpace(v)
-		}
-	}
-	return ""
-}
-
-func cloneAnyMap(in map[string]any) map[string]any {
-	if len(in) == 0 {
-		return nil
-	}
-	out := make(map[string]any, len(in))
-	for k, v := range in {
-		out[k] = v
-	}
-	return out
-}
-
-func cloneStringMap(in map[string]string) map[string]string {
-	if len(in) == 0 {
-		return nil
-	}
-	out := make(map[string]string, len(in))
-	for k, v := range in {
-		out[k] = v
-	}
-	return out
-}
-
-func intPtr(v int) *int {
-	return &v
-}
+var (
+	firstNonEmpty  = primitives.FirstNonEmpty
+	cloneAnyMap    = primitives.CloneAnyMapNilOnEmpty
+	cloneStringMap = primitives.CloneStringMapNilOnEmpty
+	intPtr         = primitives.Int
+)
