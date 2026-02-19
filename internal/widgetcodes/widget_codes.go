b41d60398171aaa7c9815f0b5c7f1dd8b5452dec
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-19 15:58:20 -0800

    add: internal widget codes

---
+++ internal/widgetcodes/widget_codes.go
@@ -0,0 +1,22 @@
+package widgetcodes
+
+// Canonical dashboard widget definition codes.
+const (
+	WidgetUserStats           = "admin.widget.user_stats"
+	WidgetActivityFeed        = "admin.widget.activity_feed"
+	WidgetUserActivityFeed    = "admin.widget.user_activity_feed"
+	WidgetQuickActions        = "admin.widget.quick_actions"
+	WidgetNotifications       = "admin.widget.notifications"
+	WidgetSettingsOverview    = "admin.widget.settings_overview"
+	WidgetUserProfileOverview = "admin.widget.user_profile_overview"
+	WidgetTranslationProgress = "admin.widget.translation_progress"
+	WidgetContentStats        = "admin.widget.content_stats"
+	WidgetStorageStats        = "admin.widget.storage_stats"
+	WidgetSystemHealth        = "admin.widget.system_health"
+	WidgetChartSample         = "admin.widget.chart_sample"
+	WidgetBarChart            = "admin.widget.bar_chart"
+	WidgetLineChart           = "admin.widget.line_chart"
+	WidgetPieChart            = "admin.widget.pie_chart"
+	WidgetGaugeChart          = "admin.widget.gauge_chart"
+	WidgetScatterChart        = "admin.widget.scatter_chart"
+)
