77d3c7298e410bb315c017f8f9c3059f43c2f6ad
goliatone <140582+goliatone@users.noreply.github.com>
2026-02-19 15:58:07 -0800

    add: widget codes

---
+++ admin/widget_codes.go
@@ -0,0 +1,24 @@
+package admin
+
+import "github.com/goliatone/go-admin/internal/widgetcodes"
+
+// Canonical dashboard widget definition codes.
+const (
+	WidgetUserStats           = widgetcodes.WidgetUserStats
+	WidgetActivityFeed        = widgetcodes.WidgetActivityFeed
+	WidgetUserActivityFeed    = widgetcodes.WidgetUserActivityFeed
+	WidgetQuickActions        = widgetcodes.WidgetQuickActions
+	WidgetNotifications       = widgetcodes.WidgetNotifications
+	WidgetSettingsOverview    = widgetcodes.WidgetSettingsOverview
+	WidgetUserProfileOverview = widgetcodes.WidgetUserProfileOverview
+	WidgetTranslationProgress = widgetcodes.WidgetTranslationProgress
+	WidgetContentStats        = widgetcodes.WidgetContentStats
+	WidgetStorageStats        = widgetcodes.WidgetStorageStats
+	WidgetSystemHealth        = widgetcodes.WidgetSystemHealth
+	WidgetChartSample         = widgetcodes.WidgetChartSample
+	WidgetBarChart            = widgetcodes.WidgetBarChart
+	WidgetLineChart           = widgetcodes.WidgetLineChart
+	WidgetPieChart            = widgetcodes.WidgetPieChart
+	WidgetGaugeChart          = widgetcodes.WidgetGaugeChart
+	WidgetScatterChart        = widgetcodes.WidgetScatterChart
+)
