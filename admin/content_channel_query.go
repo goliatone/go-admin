244115db6e2fc6ba68366bc4bf18fa6b1b4ea0b2
goliatone <140582+goliatone@users.noreply.github.com>
2026-03-03 19:33:11 -0800

    add: content channel query

---
+++ admin/content_channel_query.go
@@ -0,0 +1,5 @@
+package admin
+
+// ContentChannelScopeQueryParam is the canonical query-string key used to carry
+// content channel scope through list/read routes.
+const ContentChannelScopeQueryParam = "$channel"
