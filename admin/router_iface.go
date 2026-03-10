07279bb6c4138e57cd77682c6c841d613f1b9e56
goliatone <140582+goliatone@users.noreply.github.com>
2026-03-10 14:01:15 -0700

    add: udpated router interfaces

--- admin/router_iface.go
+++ admin/router_iface.go
@@ -2,10 +2,13 @@ package admin
 
 import router "github.com/goliatone/go-router"
 
-// AdminRouter is the minimal subset of go-router Router needed by admin.
+// AdminRouter exposes an expressive routing surface used by admin and quickstart.
 type AdminRouter interface {
+	Handle(method router.HTTPMethod, path string, handler router.HandlerFunc, mw ...router.MiddlewareFunc) router.RouteInfo
 	Get(path string, handler router.HandlerFunc, mw ...router.MiddlewareFunc) router.RouteInfo
 	Post(path string, handler router.HandlerFunc, mw ...router.MiddlewareFunc) router.RouteInfo
 	Put(path string, handler router.HandlerFunc, mw ...router.MiddlewareFunc) router.RouteInfo
 	Delete(path string, handler router.HandlerFunc, mw ...router.MiddlewareFunc) router.RouteInfo
+	Patch(path string, handler router.HandlerFunc, mw ...router.MiddlewareFunc) router.RouteInfo
+	Head(path string, handler router.HandlerFunc, mw ...router.MiddlewareFunc) router.RouteInfo
 }
