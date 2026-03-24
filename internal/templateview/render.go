b917ab01c209926f03c2e30879941e3de40a59c6
goliatone <140582+goliatone@users.noreply.github.com>
2026-03-24 15:26:54 -0700

    add: integreate CSRF token with login setup

--- internal/templateview/render.go
+++ internal/templateview/render.go
@@ -1,6 +1,7 @@
 package templateview
 
 import (
+	csrfmw "github.com/goliatone/go-auth/middleware/csrf"
 	router "github.com/goliatone/go-router"
 )
 
@@ -9,6 +10,16 @@ import (
 func RenderTemplateView(c router.Context, template string, viewCtx router.ViewContext) error {
 	if viewCtx == nil {
 		viewCtx = router.ViewContext{}
+	}
+	if c != nil {
+		if helpers, ok := c.Locals(csrfmw.DefaultTemplateHelpersKey).(map[string]any); ok && helpers != nil {
+			viewCtx[csrfmw.DefaultTemplateHelpersKey] = helpers
+			for key, value := range helpers {
+				if _, exists := viewCtx[key]; !exists {
+					viewCtx[key] = value
+				}
+			}
+		}
 	}
 	NormalizeContextNumbers(viewCtx)
 	return c.Render(template, viewCtx)
