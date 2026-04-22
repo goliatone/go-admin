0e5ecfae8af33e2dce4da04cc9ad986de63adb26
goliatone <140582+goliatone@users.noreply.github.com>
2026-04-22 15:19:40 -0700

    add: cms block definition channel

--- admin/cms_content_type_compat.go
+++ admin/cms_content_type_compat.go
@@ -5,3 +5,7 @@ import cmsadapter "github.com/goliatone/go-admin/admin/internal/cmsadapter"
 func CMSContentTypeChannel(ct CMSContentType) string {
 	return cmsadapter.ContentTypeChannel(ct)
 }
+
+func CMSBlockDefinitionChannel(def CMSBlockDefinition) string {
+	return cmsadapter.BlockDefinitionChannel(def)
+}
