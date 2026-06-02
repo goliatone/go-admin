f81c992dee16bd07a0ba4bac1d0e51206624f919
goliatone <140582+goliatone@users.noreply.github.com>
2026-06-02 15:05:06 -0700

    fix: expose interfaces

--- admin/cms_gocms_translation_adapter.go
+++ admin/cms_gocms_translation_adapter.go
@@ -2,11 +2,10 @@ package admin
 
 import (
 	"context"
-	"github.com/goliatone/go-admin/admin/cms/gocmsutil"
+
 	cmsadapter "github.com/goliatone/go-admin/admin/internal/cmsadapter"
 	cms "github.com/goliatone/go-cms"
 	"github.com/google/uuid"
-	"reflect"
 )
 
 // CreateTranslation attempts to use a first-class go-cms translation command when available.
@@ -50,48 +49,12 @@ func (a *GoCMSContentAdapter) CreateTranslation(ctx context.Context, input Trans
 		converted := a.convertAdminContentRecord(ctx, *record)
 		return &converted, nil
 	}
-	record, err := a.createTranslationRecord(ctx, input)
+	record, err := a.createTranslationRecordViaLegacyReflection(ctx, input)
 	if err != nil {
 		return nil, err
 	}
 	converted := a.convertContent(ctx, record, input.Locale)
 	return &converted, nil
-}
-
-func (a *GoCMSContentAdapter) createTranslationRecord(ctx context.Context, input TranslationCreateInput) (reflect.Value, error) {
-	var method reflect.Value
-	if a.translations != nil {
-		method = reflect.ValueOf(a.translations).MethodByName("CreateTranslation")
-	}
-	if !method.IsValid() {
-		method = reflect.ValueOf(a.content).MethodByName("CreateTranslation")
-	}
-	if !method.IsValid() {
-		return reflect.Value{}, ErrTranslationCreateUnsupported
-	}
-	sourceID := cmsadapter.UUIDFromString(input.SourceID)
-	if sourceID == uuid.Nil {
-		return reflect.Value{}, ErrNotFound
-	}
-	args, err := buildCreateTranslationMethodArgs(method, ctx, input, sourceID)
-	if err != nil {
-		return reflect.Value{}, err
-	}
-	results := method.Call(args)
-	if len(results) == 0 {
-		return reflect.Value{}, ErrNotFound
-	}
-	if last := results[len(results)-1]; last.IsValid() && last.Type().Implements(reflect.TypeFor[error]()) && !last.IsNil() {
-		if typedErr, ok := last.Interface().(error); ok {
-			return reflect.Value{}, normalizeGoCMSTranslationCreateError(typedErr, input)
-		}
-		return reflect.Value{}, ErrNotFound
-	}
-	record := gocmsutil.Deref(results[0])
-	if !record.IsValid() {
-		return reflect.Value{}, ErrNotFound
-	}
-	return record, nil
 }
 
 func shouldUseAdminContentTranslationCreate(TranslationCreateInput) bool {
